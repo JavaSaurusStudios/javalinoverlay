2c086ebeb75887c4a2b6fce3634a9df9f684a90b
JavaSaurusStudios <98191221+JavaSaurusStudios@users.noreply.github.com>
2023-04-22 17:05:42 +0200

    Fix always true error

--- twitch/scripts/response/message.js
+++ twitch/scripts/response/message.js
@@ -1,10 +1,12 @@
-function EvaluateMessage(tags,message) {
-    console.log(tags['color']+":"+tags['display-name']+":"+ message);
+function EvaluateMessage(tags, message, allowCommands) {
+    console.log(tags['color'] + ":" + tags['display-name'] + ":" + message);
     if (hasProfanity(message)) {
         playProfanityAudio();
     } else if (message.startsWith("!")) {
-        var sound = "./twitch/audio/" + message.replace("!", "") + ".mp3";
-        playChatAudio(sound);
-        console.log("Playing " + sound);
+        if (allowCommands) {
+            var sound = "./twitch/audio/chat-alert/" + message.replace("!", "") + ".mp3";
+            playChatAudio(sound);
+            console.log("Playing " + sound);
+        }
     }
 }
